6727f871cd09341d0992b2dfa4f0ff6c3d031d1d
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-02-17 20:10:54 -0500

    move class into its own file

---
+++ ToolCharging/ModConfig.cs
@@ -0,0 +1,22 @@
+﻿namespace ToolCharging
+{
+    internal class ModConfig
+    {
+        private int _increaseBy;
+        public int IncreaseBy
+        {
+            get { return _increaseBy; }
+            set
+            {
+                if (value < -15) { _increaseBy = -15; }
+                else if (value > 599) { _increaseBy = 599; }
+                else { _increaseBy = value; }
+            }
+        }
+
+        public ModConfig()
+        {
+            this._increaseBy = 40;
+        }
+    }
+}
--- ToolCharging/ToolCharging.cs
+++ ToolCharging/ToolCharging.cs
@@ -48,24 +48,4 @@ namespace ToolCharging
                 Game1.player.toolHold -= extraRemove;
         }
     }
-
-    public class ModConfig
-    {
-        private int _increaseBy;
-        public int IncreaseBy
-        {
-            get { return _increaseBy; }
-            set
-            {
-                if (value < -15) { _increaseBy = -15; }
-                else if (value > 599) { _increaseBy = 599; }
-                else { _increaseBy = value; }
-            }
-        }
-
-        public ModConfig()
-        {
-            this._increaseBy = 40;
-        }
-    }
 }
